c933549694470bea299a03b62faeae97fdeabbdb
hzy <farawayfed@gmail.com>
2022-09-25 03:16:56 +0800

    fix: broken links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/Tutorial',
               },
             ],
           },
