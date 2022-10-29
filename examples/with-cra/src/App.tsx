79382dbf8327d0bf2090f1fb0f3453325f649743
LLGZONE <985395976@qq.com>
2022-10-29 23:31:38 +0800

    feat: add example

--- examples/with-cra/src/App.tsx
+++ examples/with-cra/src/App.tsx
@@ -1,9 +1,14 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 import logoSuiet from './suiet-logo.svg';
-import {ConnectButton, getDefaultWallets, useWallet, WalletProvider} from '@suiet/wallet-kit'
-import "@suiet/wallet-kit/style.css";
+import {
+  ConnectButton,
+  getDefaultWallets,
+  useWallet,
+  WalletProvider,
+} from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
 
 function App() {
   const {
@@ -11,75 +16,86 @@ function App() {
     connected,
     connecting,
     getAccounts,
-    executeMoveCall,
+    signAndExecuteTransaction,
+    getPublicKey,
   } = useWallet();
-
-  const [accounts, setAccounts] = useState<string[]>([])
+  const [pbk, setpbk] = useState('');
+  const [accounts, setAccounts] = useState<string[]>([]);
 
   useEffect(() => {
-    if (!connected) return
+    if (!connected) return;
     (async function () {
       const result = await getAccounts();
       setAccounts(result);
-    })()
-  }, [connected])
+    })();
+  }, [connected]);
 
   async function handleExecuteMoveCall() {
     try {
       const data = {
-        packageObjectId: "0x2",
-        module: "devnet_nft",
-        function: "mint",
+        packageObjectId: '0x2',
+        module: 'devnet_nft',
+        function: 'mint',
         typeArguments: [],
         arguments: [
-          "name",
-          "capy",
-          "https://cdn.britannica.com/94/194294-138-B2CF7780/overview-capybara.jpg?w=800&h=450&c=crop",
+          'name',
+          'capy',
+          'https://cdn.britannica.com/94/194294-138-B2CF7780/overview-capybara.jpg?w=800&h=450&c=crop',
         ],
         gasBudget: 10000,
-      }
-      const resData = await executeMoveCall(data);
-      console.log('executeMoveCall success', resData)
-      alert('executeMoveCall succeeded (see response in the console)')
+      };
+      const resData = await signAndExecuteTransaction({
+        kind: 'moveCall',
+        data,
+      });
+      console.log('executeMoveCall success', resData);
+      alert('executeMoveCall succeeded (see response in the console)');
     } catch (e) {
-      console.error('executeMoveCall failed', e)
-      alert('executeMoveCall failed (see response in the console)')
+      console.error('executeMoveCall failed', e);
+      alert('executeMoveCall failed (see response in the console)');
     }
   }
+
+  const publicKey = async () => {
+    const k = await getPublicKey();
+    console.log(k);
+    setpbk(k);
+  };
 
   return (
     <div className="App">
       <header className="header">
-        <img src={logoSuiet} className="logo-suiet" alt="suiet-logo"/>
+        <img src={logoSuiet} className="logo-suiet" alt="suiet-logo" />
         <div className={'connect-button'}>
-          <ConnectButton style={{width: '100px'}}/>
+          <ConnectButton style={{ width: '100px' }} />
         </div>
       </header>
       <main className={'main'}>
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         {!connected ? (
-          <p>
-            Connect DApp with Suiet wallet from now!
-          </p>
+          <p>Connect DApp with Suiet wallet from now!</p>
         ) : (
           <div>
             <div>
-              <p>current wallet: {wallet ? wallet.adapter.name : "null"}</p>
+              <p>current wallet: {wallet ? wallet.name : 'null'}</p>
               <p>
-                wallet status:{" "}
+                wallet status:{' '}
                 {connecting
-                  ? "connecting"
+                  ? 'connecting'
                   : connected
-                    ? "connected"
-                    : "disconnected"}
+                  ? 'connected'
+                  : 'disconnected'}
               </p>
               <p>wallet accounts: {JSON.stringify(accounts)}</p>
             </div>
-            <div style={{margin: "8px 0"}}>
+            <div style={{ margin: '8px 0' }}>
               <button onClick={handleExecuteMoveCall}>executeMoveCall</button>
             </div>
+            <div style={{ margin: '8px 0' }}>
+              <button onClick={publicKey}>pbk</button>
+              <div>public key: {pbk}</div>
+            </div>
           </div>
-
         )}
         <a
           className="App-link"
@@ -97,7 +113,7 @@ function App() {
 export default function withApp() {
   return (
     <WalletProvider supportedWallets={getDefaultWallets()}>
-      <App/>
+      <App />
     </WalletProvider>
-  )
-};
+  );
+}
