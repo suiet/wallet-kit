f31a618850c90be7e577fc5e466524dbeec84c4a
brrruski <sizhe4real@gmail.com>
2022-11-02 16:30:39 -0400

    build: change bundle tool to rollup

---
+++ packages/kit/rollup.config.js
@@ -0,0 +1,55 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+import postcss from "rollup-plugin-postcss";
+import {visualizer} from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
+import svg from 'rollup-plugin-svg'
+import cleaner from 'rollup-plugin-cleaner';
+import {summary} from "rollup-plugin-summary";
+
+import * as path from 'path'
+import {defineConfig} from "rollup";
+
+export default defineConfig({
+  input: './src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    sourcemap: true,
+  },
+  plugins: [
+    cleaner({
+      targets: [
+        './dist'
+      ]
+    }),
+    resolve({
+      browser: true, // specify that it's built for browser
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
+    svg(),
+    postcss({
+      extract: path.resolve('dist/style.css')
+    }),
+    terser(),
+    visualizer({
+      filename: 'bundle-analysis.html',
+      open: true,
+    }),
+    summary({
+      warnLow: 1000,
+      warnHigh: 3000,
+      showMinifiedSize: false,
+    }),
+  ],
+  external: ['react', 'react-dom'],
+})
