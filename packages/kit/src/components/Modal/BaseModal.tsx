983220b4092c1e8de0889d47fe2654fa2bff6088
brrruski <sizhe4real@gmail.com>
2022-11-03 23:36:18 -0400

    refactor(kit): button & modal

---
+++ packages/kit/src/components/Modal/BaseModal.tsx
@@ -0,0 +1,35 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import * as React from "react";
+import {Extendable} from "../../types";
+import classnames from "classnames";
+import {DialogContentProps} from "@radix-ui/react-dialog";
+
+export type BaseModalProps = Extendable & {
+  trigger?: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  contentProps?: DialogContentProps;
+}
+
+export const BaseModal = (props: BaseModalProps) => {
+  const {
+    open = false,
+  } = props;
+
+  return (
+    <Dialog.Root open={open} onOpenChange={props.onOpenChange}>
+      <Dialog.Trigger asChild>{props.trigger}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className={'wkit-dialog__overlay'}>
+          <Dialog.Content
+            {...props.contentProps}
+            className={classnames('wkit-dialog__content', props.className)}
+            style={props.style}
+          >
+            {props.children}
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
--- packages/kit/src/wallet-standard/use-wallet-detection.ts
+++ packages/kit/src/wallet-standard/use-wallet-detection.ts
@@ -1,28 +1,29 @@
 import {useEffect, useRef, useState} from "react";
-import {DEPRECATED_getWallets, Wallet} from '@wallet-standard/core'
+import {DEPRECATED_getWallets, Wallet, Wallets} from '@wallet-standard/core'
 import {IWalletAdapter} from "../types/wallet";
 import {WalletAdapter} from "../wallet/wallet-adapter";
+import {isEmpty} from "lodash-es";
 
 /**
  * detect wallet adapters that support wallet-standard from window and register event
  * normalize them to WalletAdapter
  */
 export function useWalletAdapterDetection() {
-  const standardWalletManager = useRef(DEPRECATED_getWallets())
+  const standardWalletManager = useRef<Wallets>()
   // normalized adapters
   const [availableWalletAdapters, setAvailableWalletAdapters] = useState<IWalletAdapter[]>([])
   console.log('availableWalletAdapters', availableWalletAdapters)
 
-  function normalizeWalletAdapter(adapter: Wallet) {
-    return new WalletAdapter(adapter)
-  }
-
   useEffect(() => {
+    standardWalletManager.current = DEPRECATED_getWallets();
     const initWalletAdapters = standardWalletManager.current.get()
-    console.log('initWalletAdapters', initWalletAdapters)
 
+    if (!isEmpty(initWalletAdapters)) {
+      setAvailableWalletAdapters(initWalletAdapters.map((wallet => new WalletAdapter(wallet))))
+    }
     const clearListeners = standardWalletManager.current.on('register', (...wallets: Wallet[]) => {
-      setAvailableWalletAdapters(wallets.map(normalizeWalletAdapter))
+      // normalize to WalletAdapter
+      setAvailableWalletAdapters(wallets.map((wallet => new WalletAdapter(wallet))))
     })
     return () => {
       clearListeners()
