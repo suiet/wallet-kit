3e430590e60544be4a22989637c4f3d331d76bc6
brrruski <sizhe4real@gmail.com>
2022-11-12 16:45:28 -0500

    feat(kit): detect new registered wallets and merge them without duplicated and invalid ones.

--- packages/kit/src/utils/check.ts
+++ packages/kit/src/utils/check.ts
@@ -1,3 +1,15 @@
+import {StandardWalletAdapterWallet, Wallet} from "@mysten/wallet-standard";
+
 export function isNonEmptyArray(value: any): boolean {
   return Array.isArray(value) && value.length > 0;
+}
+
+export function isStandardWalletAdapterCompatibleWallet(
+  wallet: Wallet
+): wallet is StandardWalletAdapterWallet {
+  return (
+    "standard:connect" in wallet.features &&
+    "standard:events" in wallet.features &&
+    "sui:signAndExecuteTransaction" in wallet.features
+  );
 }
