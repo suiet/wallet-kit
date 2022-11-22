188885f41e688aa7609e64f76e10ce35294275bd
brrruski <sizhe4real@gmail.com>
2022-11-22 10:34:11 -0500

    chore(kit): adjust code

--- packages/kit/src/components/WalletProvider.tsx
+++ packages/kit/src/components/WalletProvider.tsx
@@ -3,7 +3,7 @@ import {WalletContext} from "../hooks/useWallet";
 import {
   ConnectionStatus,
   IWalletAdapter,
-  IDefaultWallet, IWallet,
+  IDefaultWallet,
 } from "../types/wallet";
 import {
   ConnectInput,
@@ -12,13 +12,13 @@ import {
 } from "@mysten/wallet-standard";
 import {KitError} from "../errors";
 import {AllDefaultWallets} from "../wallet/default-wallets";
-import {useWalletAdapterDetection} from "../wallet-standard/use-wallet-detection";
 import { Extendable } from '../types/utils';
 import {isNonEmptyArray} from "../utils";
 import {MoveCallTransaction, SuiTransactionResponse} from "@mysten/sui.js";
 import {FeatureName} from "../wallet/wallet-adapter";
 import {deprecatedWarn} from "../legacy/tips";
 import {WalletEvent, WalletEventListeners} from "../types/events";
+import {useAvailableWallets} from "../hooks/useAvaibleWallets";
 
 export type WalletProviderProps = Extendable & {
   defaultWallets?: IDefaultWallet[];
@@ -26,71 +26,6 @@ export type WalletProviderProps = Extendable & {
    * @deprecated use defaultWallets to customize wallet list
    */
   supportedWallets?: any[];
-};
-
-const useAvailableWallets = (defaultWallets: IDefaultWallet[]) => {
-  const {data: availableWalletAdapters} = useWalletAdapterDetection()
-  // configured wallets
-  const configuredWallets = useMemo(() => {
-    if (!isNonEmptyArray(defaultWallets)) return [];
-    if (!isNonEmptyArray(availableWalletAdapters)) {
-      return defaultWallets.map(item => ({
-        ...item,
-        adapter: undefined,
-        installed: false,
-      }) as IWallet)
-    }
-
-    return defaultWallets.map((item) => {
-      const foundAdapter = availableWalletAdapters.find(walletAdapter => item.name === walletAdapter.name);
-      if (foundAdapter) {
-        return {
-          ...item,
-          adapter: foundAdapter,
-          installed: true,
-        } as IWallet
-      }
-      return {
-        ...item,
-        adapter: undefined,
-        installed: false,
-      } as IWallet
-    });
-  }, [defaultWallets, availableWalletAdapters])
-
-  // detected wallets
-  const detectedWallets = useMemo(() => {
-    if (!isNonEmptyArray(availableWalletAdapters)) return [];
-    return availableWalletAdapters.filter(adapter => {
-      // filter adapters not shown in the configured list
-      return !defaultWallets.find(wallet => wallet.name === adapter.name)
-    }).map((adapter) => {
-      // normalized detected adapter to IWallet
-      return {
-        name: adapter.name,
-        adapter: adapter,
-        installed: true,
-        iconUrl: adapter.icon,
-        downloadUrl: {
-          browserExtension: '',  // no need to know
-        },
-      }
-    })
-  }, [defaultWallets, availableWalletAdapters]);
-
-  // filter installed wallets
-  const allAvailableWallets = useMemo(() => {
-    return [
-      ...configuredWallets,
-      ...detectedWallets,
-    ].filter(wallet => wallet.installed)
-  }, [configuredWallets, detectedWallets])
-
-  return {
-    allAvailableWallets,
-    configuredWallets,
-    detectedWallets,
-  };
 };
 
 export const WalletProvider = (props: WalletProviderProps) => {
