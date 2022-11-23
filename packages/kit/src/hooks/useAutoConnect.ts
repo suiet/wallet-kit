6b4ac310e702d48fed6723b970827141258d5fe5
brrruski <sizhe4real@gmail.com>
2022-11-23 12:18:21 -0500

    feat(kit): configure autoConnect

--- packages/kit/src/hooks/useAutoConnect.ts
+++ packages/kit/src/hooks/useAutoConnect.ts
@@ -4,19 +4,26 @@ import {IWallet} from "../types";
 import {Storage} from "../utils/storage";
 import {StorageKey} from "../constants/storage";
 
-export function useAutoConnect(select: (name: string)=> Promise<void>, allAvailableWallets: IWallet[], ) {
+export function useAutoConnect(
+  select: (name: string) => Promise<void>,
+  allAvailableWallets: IWallet[],
+  autoConnect: boolean,
+) {
   const init = useRef(false)
   const storage = useRef(new Storage())
 
   // auto connect
   useEffect(() => {
-    if (init.current) return
-    if (!isNonEmptyArray(allAvailableWallets)) return
-    const lastConnectedWalletName = storage.current.getItem(StorageKey.LAST_CONNECT_WALLET_NAME);
+    if (!autoConnect ||
+      init.current ||
+      !isNonEmptyArray(allAvailableWallets)
+    ) return
+
+    const lastConnectedWalletName = storage.current.getItem(StorageKey.LAST_CONNECT_WALLET_NAME)
     if (!lastConnectedWalletName) return
 
     if (allAvailableWallets.find(item => item.name == lastConnectedWalletName)) {
-      select(lastConnectedWalletName);
+      select(lastConnectedWalletName)
       init.current = true
     }
   }, [allAvailableWallets])
