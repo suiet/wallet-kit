d7e733816874907ce74b97c5670b7a5d84270f1a
brrruski <sizhe4real@gmail.com>
2022-11-26 14:45:28 -0500

    docs: update docs for chain

--- examples/with-vite/src/main.tsx
+++ examples/with-vite/src/main.tsx
@@ -5,15 +5,34 @@ import './index.css'
 
 import {
   WalletProvider,
+  Chain,
+  SuiDevnetChain,
+  SuiTestnetChain,
+  DefaultChains,
 } from '@suiet/wallet-kit';
+
+const customChain: Chain = {
+  id: "",
+  name: "",
+  rpcUrl: ""
+}
+
+const SupportedChains: Chain[] = [
+  // ...DefaultChains,
+  SuiDevnetChain,
+  SuiTestnetChain,
+  // NOTE: you can add custom chain (network),
+  // but make sure the connected wallet does support it
+  // customChain,
+]
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <WalletProvider>
+    <WalletProvider chains={SupportedChains}>
       {/* if you want to custiomize you wallet list, please check this doc
           https://kit.suiet.app/docs/components/WalletProvider#customize-your-wallet-list-on-modal
        */}
-      <App />
+      <App/>
     </WalletProvider>
   </React.StrictMode>
 )
