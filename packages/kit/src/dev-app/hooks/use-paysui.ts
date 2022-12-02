7a10f5297010c085f820e72015e8b6b5e53c22f9
brrruski <sizhe4real@gmail.com>
2022-12-02 16:41:57 -0500

    chore(kit): add paySui dev workaround

---
+++ packages/kit/src/dev-app/hooks/use-paysui.ts
@@ -0,0 +1,76 @@
+import {useSuiProvider, useWallet} from "../../hooks";
+import {useCallback} from "react";
+import {Coin, getMoveObject, getObjectExistsResponse, SuiMoveObject, SuiObject} from "@mysten/sui.js";
+import {isNonEmptyArray} from "../../utils";
+
+
+export function usePaysuiDev() {
+  const {signAndExecuteTransaction, chain} = useWallet()
+  const sui = useSuiProvider(chain?.rpcUrl ?? '')
+
+  const getOwnedObjects = useCallback(async (address: string) => {
+    const objects = await sui.getObjectsOwnedByAddress(address);
+    if (!isNonEmptyArray(objects)) return []
+    const objectIds = objects.map(o => o.objectId)
+    const res = await sui.getObjectBatch(objectIds);
+    return res.filter(item => item.status === 'Exists')
+      .map(item => (getObjectExistsResponse(item)) as SuiObject)
+  }, [sui])
+
+  const getOwnedCoins = useCallback(async (address: string) => {
+    const objects = await getOwnedObjects(address);
+    return objects.map(coin => ({
+        id: coin.reference.objectId,
+        object: getMoveObject(coin) as SuiMoveObject,
+      }))
+  }, [getOwnedObjects])
+
+  const getOwnedCoinSui = useCallback(async (address: string) => {
+    const coins = await getOwnedCoins(address)
+    return coins.filter(coin => Coin.isSUI(coin.object))
+      .map(coin => coin.object)
+  }, [getOwnedCoins])
+
+
+  const paysuiDev = useCallback(async (params: {
+      address: string;
+      recipients: string[];
+      amounts: number[];
+      gasBudget?: number,
+  }) => {
+    const {
+      address,
+      recipients,
+      amounts,
+      gasBudget = 100,
+    } = params;
+    const ownedSuiCoins = await getOwnedCoinSui(address);
+    console.log('ownedSuiCoins=', ownedSuiCoins)
+    // const ownedSuiData = ownedSui.map(o => o.)
+    const totalAmount = amounts.reduce((pre, cur) => pre + cur, 0)
+    console.log('totalAmount=', totalAmount)
+    const inputCoins = await Coin.selectCoinSetWithCombinedBalanceGreaterThanOrEqual(ownedSuiCoins, BigInt(totalAmount));
+    console.log('inputCoins=', inputCoins)
+    const inputCoinIds = inputCoins.map(c => Coin.getID(c))
+    console.log('inputCoinIds=', inputCoinIds)
+    const data: any = {
+      transaction: {
+        kind: "paySui",
+        data: {
+          // The first SUI coin object input will be used for gas payment,
+          // so the balance of this SUI coin has to be equal to or greater than the gas budget.
+          inputCoins: inputCoinIds,
+          recipients,
+          amounts,
+          gasBudget: 1000,
+        }
+      }
+    }
+    console.log('signAndExecuteTransaction data=', data)
+    const res = await signAndExecuteTransaction(data)
+    console.log('signAndExecuteTransaction res=', res)
+    return res
+  }, [signAndExecuteTransaction])
+
+  return {paySui: paysuiDev}
+}
--- packages/kit/src/main.tsx
+++ packages/kit/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import ReactDOM from 'react-dom/client';
 import { WalletProvider, ConnectButton } from './components';
 import {useWallet} from "./hooks";
+import {usePaysuiDev} from "./dev-app/hooks/use-paysui";
 
 function App() {
   const wallet = useWallet()
+  const {paySui} = usePaysuiDev()
+
+  const handlePaySui = useCallback(async () => {
+    if (!wallet.address) return
+
+    await paySui({
+      address: wallet.address,
+      recipients: ['0xdd54b1b033df2ee7acf15924eed369e5405af406'],
+      amounts: [1000],
+    })
+  }, [wallet, paySui])
 
   return (
     <div style={{
@@ -32,6 +44,9 @@ function App() {
             </p>
             <p>wallet address: {wallet.account?.address}</p>
             <p>current network: {wallet.chain?.name}</p>
+          </div>
+          <div>
+            <button onClick={handlePaySui}>PaySui</button>
           </div>
         </div>
       )}
