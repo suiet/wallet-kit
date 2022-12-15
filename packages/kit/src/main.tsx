baa4b1d7a8734c7157f387e0952c9d61c1504861
brrruski <sizhe4real@gmail.com>
2022-12-15 15:38:31 -0500

    feat: upgrade sui.js to 0.20.0

--- packages/kit/src/main.tsx
+++ packages/kit/src/main.tsx
@@ -2,10 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WalletProvider, ConnectButton } from './components';
 import {useWallet} from "./hooks";
+import * as tweetnacl from 'tweetnacl';
 
 function App() {
   const wallet = useWallet()
+  async function handleExecuteMoveCall() {
+    try {
+      const data = {
+        packageObjectId: '0x2',
+        module: 'devnet_nft',
+        function: 'mint',
+        typeArguments: [],
+        arguments: [
+          'name',
+          'capy',
+          'https://cdn.britannica.com/94/194294-138-B2CF7780/overview-capybara.jpg?w=800&h=450&c=crop',
+        ],
+        gasBudget: 10000,
+      };
+      const resData = await wallet.signAndExecuteTransaction({
+        transaction: {
+          kind: 'moveCall',
+          data
+        }
+      });
+      // const resData = await executeMoveCall(data);
+      console.log('executeMoveCall success', resData);
+      alert('executeMoveCall succeeded (see response in the console)');
+    } catch (e) {
+      console.error('executeMoveCall failed', e);
+      alert('executeMoveCall failed (see response in the console)');
+    }
+  }
 
+  async function handleSignMsg() {
+    try {
+      const msg = 'Hello world!'
+      const result = await wallet.signMessage({
+        message: new TextEncoder().encode('Hello world')
+      })
+      if (!result) {
+        alert('signMessage return null')
+        return
+      }
+      console.log('send message to be signed', msg)
+      const textDecoder = new TextDecoder()
+      console.log('signMessage success', result)
+      console.log('signMessage signature', result.signature)
+      console.log('signMessage signedMessage', textDecoder.decode(result.signedMessage).toString())
+      console.log('verify via tweetnacl', tweetnacl.sign.detached.verify(
+        result.signedMessage,
+        result.signature,
+        wallet.account?.publicKey as Uint8Array,
+      ))
+      alert('signMessage succeeded (see response in the console)')
+    } catch (e) {
+      console.error('signMessage failed', e)
+      alert('signMessage failed (see response in the console)')
+    }
+  }
   return (
     <div style={{
       height: '100vh',
@@ -19,7 +74,7 @@ function App() {
       {!wallet.connected ? (
         <p>Connect DApp with Suiet wallet from now!</p>
       ) : (
-        <div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <div style={{textAlign: 'center'}}>
             <p>current wallet: {wallet.adapter?.name}</p>
             <p>
@@ -32,6 +87,10 @@ function App() {
             </p>
             <p>wallet address: {wallet.account?.address}</p>
             <p>current network: {wallet.chain?.name}</p>
+          </div>
+          <div style={{ margin: '8px 0' }}>
+            <button onClick={handleExecuteMoveCall}>executeMoveCall</button>
+            <button style={{ marginLeft: '8px' }} onClick={handleSignMsg}>signMessage</button>
           </div>
         </div>
       )}
