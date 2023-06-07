c4cf613ac975315edadadff4ed96b9602a7f0bdc
brrruski <sizhe4real@gmail.com>
2023-06-07 13:15:19 -0400

    example: upgrade demo

--- examples/with-next/components/App.tsx
+++ examples/with-next/components/App.tsx
@@ -8,7 +8,8 @@ import {TransactionBlock} from "@mysten/sui.js";
 import {useMemo} from "react";
 
 const sampleNft = new Map([
-  ['sui:testnet', '0x57c53166c2b04c1f1fc93105b39b6266cb1eccbe654f5d2fc89d5b44524b11fd::nft::mint'],
+  ['sui:devnet', '0xe146dbd6d33d7227700328a9421c58ed34546f998acdc42a1d05b4818b49faa2::nft::mint'],
+  ['sui:testnet', '0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint'],
   ['sui:mainnet', '0x5b45da03d42b064f5e051741b6fed3b29eb817c7923b83b92f37a1d2abf4fbab::nft::mint'],
 ])
 
