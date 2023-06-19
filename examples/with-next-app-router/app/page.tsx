3f59d4d20e23493debbfcf269a8bde2492bb877b
brrruski <sizhe4real@gmail.com>
2023-06-19 13:09:46 -0400

    example: update next.js demo

--- examples/with-next-app-router/app/page.tsx
+++ examples/with-next-app-router/app/page.tsx
@@ -120,12 +120,15 @@ export default function Home() {
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
-          src="/next.svg"
-          alt="Next.js Logo"
+          className="logo relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
+          src="/suiet-logo.svg"
+          alt="Suiet Logo"
           width={180}
           height={37}
           priority
+          onClick={() => {
+            window.open('https://github.com/suiet/wallet-kit', '_blank')
+          }}
         />
 
         <ConnectButton
@@ -167,6 +170,8 @@ export default function Home() {
           </div>
         )}
       </div>
+
+      <div></div>
     </main>
   )
 }
