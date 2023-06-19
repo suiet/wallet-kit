1bb469881b45bbe3a61e0741d8cf57e170f2371a
brrruski <sizhe4real@gmail.com>
2023-06-19 12:54:08 -0400

    example: add a new example with nextjs (app router mode)

---
+++ examples/with-next-app-router/app/page.tsx
@@ -0,0 +1,172 @@
+"use client";  // In Next.js, this is required to prevent the component from being rendered on the server.
+
+import Image from 'next/image'
+import {
+  addressEllipsis,
+  ConnectButton,
+  ErrorCode,
+  formatSUI,
+  SuiChainId,
+  useAccountBalance,
+  useWallet
+} from "@suiet/wallet-kit";
+import {useMemo} from "react";
+import {TransactionBlock} from "@mysten/sui.js";
+
+const sampleNft = new Map([
+  ['sui:devnet', '0xe146dbd6d33d7227700328a9421c58ed34546f998acdc42a1d05b4818b49faa2::nft::mint'],
+  ['sui:testnet', '0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint'],
+  ['sui:mainnet', '0x5b45da03d42b064f5e051741b6fed3b29eb817c7923b83b92f37a1d2abf4fbab::nft::mint'],
+])
+
+
+export default function Home() {
+  const wallet = useWallet()
+  const {balance} = useAccountBalance();
+  const nftContractAddr = useMemo(() => {
+    if (!wallet.chain) return '';
+    return sampleNft.get(wallet.chain.id) ?? '';
+  }, [wallet]);
+
+  function uint8arrayToHex(value: Uint8Array | undefined) {
+    if (!value) return ''
+    // @ts-ignore
+    return value.toString('hex')
+  }
+
+  async function handleExecuteMoveCall(target: string | undefined) {
+    if (!target) return;
+
+    try {
+      const tx = new TransactionBlock()
+      tx.moveCall({
+        target: target as any,
+        arguments: [
+          tx.pure('Suiet NFT'),
+          tx.pure('Suiet Sample NFT'),
+          tx.pure('https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4')
+        ]
+      })
+      const resData = await wallet.signAndExecuteTransactionBlock({
+        transactionBlock: tx,
+      });
+      console.log('executeMoveCall success', resData);
+      alert('executeMoveCall succeeded (see response in the console)');
+    } catch (e) {
+      console.error('executeMoveCall failed', e);
+      alert('executeMoveCall failed (see response in the console)');
+    }
+  }
+
+  async function handleSignMsg() {
+    try {
+      const msg = 'Hello world!'
+      const msgBytes = new TextEncoder().encode(msg)
+      const result = await wallet.signMessage({
+        message: msgBytes
+      })
+      const verifyResult = wallet.verifySignedMessage(result)
+      console.log('verify signedMessage', verifyResult)
+      if (!verifyResult) {
+        alert(`signMessage succeed, but verify signedMessage failed`)
+      } else {
+        alert(`signMessage succeed, and verify signedMessage succeed!`)
+      }
+    } catch (e) {
+      console.error('signMessage failed', e)
+      alert('signMessage failed (see response in the console)')
+    }
+  }
+
+  const chainName = (chainId: string | undefined) => {
+    switch (chainId) {
+      case SuiChainId.MAIN_NET:
+        return 'Mainnet'
+      case SuiChainId.TEST_NET:
+        return 'Testnet'
+      case SuiChainId.DEV_NET:
+        return 'Devnet'
+      default:
+        return 'Unknown'
+    }
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-8"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+
+        <ConnectButton
+          onConnectError={(error) => {
+            if (error.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
+              console.warn('user rejected the connection to ' + error.details?.wallet)
+            } else {
+              console.warn('unknown connect error: ', error)
+            }
+          }}
+        />
+
+        {!wallet.connected ? (
+          <p className={'my-8'}>Connect DApp with Suiet wallet from now!</p>
+        ) : (
+          <div className={'my-8'}>
+            <div>
+              <p>current wallet: {wallet.adapter?.name}</p>
+              <p>
+                wallet status:{' '}
+                {wallet.connecting
+                  ? 'connecting'
+                  : wallet.connected
+                    ? 'connected'
+                    : 'disconnected'}
+              </p>
+              <p>wallet address: {addressEllipsis(wallet.account?.address ?? '')}</p>
+              <p>current network: {wallet.chain?.name}</p>
+              <p>wallet balance: {formatSUI(balance ?? 0, {
+                withAbbr: false
+              })} SUI</p>
+            </div>
+            <div className={'flex flex-col my-8'}>
+              {nftContractAddr && (
+                <button onClick={() => handleExecuteMoveCall(nftContractAddr)}>Mint {chainName(wallet.chain?.id)} NFT</button>
+              )}
+              <button className={'mt-4'} onClick={handleSignMsg}>signMessage</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  )
+}
