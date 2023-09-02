b6f692e7dfc8fa30344279236f5efc82479f055d
brrruski <sizhe4real@gmail.com>
2023-09-02 14:43:19 -0400

    fix: remove StandardWalletAdapterWallet type

--- packages/kit/src/utils/check.ts
+++ packages/kit/src/utils/check.ts
@@ -1,4 +1,4 @@
-import {StandardWalletAdapterWallet, Wallet} from "@mysten/wallet-standard";
+import {Wallet} from "@mysten/wallet-standard";
 
 export function isNonEmptyArray(value: any): boolean {
   return Array.isArray(value) && value.length > 0;
@@ -6,7 +6,7 @@ export function isNonEmptyArray(value: any): boolean {
 
 export function isStandardWalletAdapterCompatibleWallet(
   wallet: Wallet
-): wallet is StandardWalletAdapterWallet {
+) {
   return (
     "standard:connect" in wallet.features &&
     "standard:events" in wallet.features &&
