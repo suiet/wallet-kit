d1e60e51a8e4b83765c1dab02f196cb270d7e462
brrruski <sizhe4real@gmail.com>
2023-10-19 23:44:55 -0400

    chore(kit): update chores

--- packages/kit/src/components/WalletProvider.tsx
+++ packages/kit/src/components/WalletProvider.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo, useRef, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {WalletContext} from "../hooks";
 import type {
   StandardConnectInput,
