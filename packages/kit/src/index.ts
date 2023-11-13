3dba0866023135212f0418cfde17c4d26070eb94
brrruski <sizhe4real@gmail.com>
2023-11-12 23:36:28 -0500

    feat(kit): update exports

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./components";
 export * from "@suiet/wallet-sdk";
-// export * from './hooks';
+export * from "./hooks";
 // export * from './legacy';
-// export * from './types';
+export * from "./types";
--- packages/kit/src/types/wallet.ts
+++ packages/kit/src/types/wallet.ts
@@ -1,18 +1,4 @@
-import {
-  WalletWithFeatures,
-  StandardConnectFeature,
-  StandardConnectMethod,
-  StandardDisconnectMethod,
-  StandardEventsOnMethod,
-  SuiSignAndExecuteTransactionBlockFeature,
-  SuiSignMessageFeature,
-  SuiSignTransactionBlockFeature,
-  SuiSignAndExecuteTransactionBlockMethod,
-  SuiSignTransactionBlockMethod,
-  SuiSignMessageMethod,
-  StandardDisconnectFeature,
-  StandardEventsFeature,
-} from "@mysten/wallet-standard";
+import { IWalletAdapter } from "@suiet/wallet-sdk";
 
 export interface IWallet {
   name: string;
@@ -25,31 +11,16 @@ export interface IWallet {
   };
 }
 
-export type IDefaultWallet = Omit<IWallet,
+export type IDefaultWallet = Omit<
+  IWallet,
   keyof {
     adapter: any;
     installed: any;
-  }>;
+  }
+>;
 
 export enum ConnectionStatus {
   DISCONNECTED = "disconnected",
   CONNECTED = "connected",
   CONNECTING = "connecting",
 }
-
-export type IWalletAdapter = WalletWithFeatures<
-  StandardConnectFeature &
-  StandardEventsFeature &
-  SuiSignAndExecuteTransactionBlockFeature &
-  SuiSignTransactionBlockFeature &
-  SuiSignMessageFeature &
-  Partial<StandardDisconnectFeature>
-  > & {
-  hasFeature: (name: string) => boolean;
-  connect: StandardConnectMethod;
-  disconnect: StandardDisconnectMethod;
-  on: StandardEventsOnMethod;
-  signAndExecuteTransactionBlock: SuiSignAndExecuteTransactionBlockMethod;
-  signTransactionBlock: SuiSignTransactionBlockMethod;
-  signMessage: SuiSignMessageMethod;
-};
