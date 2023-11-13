131be7770fb20e1740a527a42782bad418b4a14a
brrruski <sizhe4real@gmail.com>
2023-11-12 22:29:57 -0500

    fix(sdk): fix bundling issues

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,7 @@
-// export * from "./account";
-// export * from "./common";
+export * from "./account";
+export * from "./common";
 export * from "./utils";
+export * from "./wallet";
 export * from "./wallet-standard";
 export * from "./chain";
+export * from "./error-handling";
--- packages/sdk/vite.config.ts
+++ packages/sdk/vite.config.ts
@@ -1,39 +1,50 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import inject from "@rollup/plugin-inject";
 
-export default defineConfig(({mode}) => ({
+export default defineConfig(({ mode }) => ({
   build: {
-    target: 'es2020',
+    target: "es2020",
+    commonjsOptions: {
+      // vite build use @rollup/plugin-commonjs as default, which transforms all the cjs files
+      // However Sui Sdk mixed using esm & cjs，therefore should turn on transformMixedEsModules.
+      // https://github.com/originjs/vite-plugins/issues/9#issuecomment-924668456
+      transformMixedEsModules: true,
+    },
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'suietWalletSdk',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "suietWalletSdk",
       // the proper extensions will be added
-      fileName: 'index',
+      fileName: "index",
     },
     esbuild: {
       target: "es2020",
-      pure: mode === 'production' ? ['console.log', 'debugger'] : [],
+      pure: mode === "production" ? ["console.log", "debugger"] : [],
     },
     optimizeDeps: {
-      esbuildOptions : {
-        target: "es2020"
-      }
+      esbuildOptions: {
+        target: "es2020",
+        define: {
+          global: "globalThis",
+        },
+      },
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['@mysten/sui.js'],
+      external: ["@mysten/sui.js"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          '@mysten/sui.js': 'Sui',
+          "@mysten/sui.js": "Sui",
         },
       },
+      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
     },
   },
   plugins: [dts()],
-}))
+}));
