5ad9309df5bcad68a5845b73f3706dbdb66103e4
brrruski <sizhe4real@gmail.com>
2023-11-12 22:33:19 -0500

    feat(kit): remove duplicate modules with wallet-sdk

--- packages/kit/src/constants/index.ts
+++ packages/kit/src/constants/index.ts
@@ -1,9 +1,7 @@
-export * from '../chain/constants'
-
 export enum QueryKey {
-  COIN_BALANCE = `SUIET_COIN_BALANCE`
+  COIN_BALANCE = `SUIET_COIN_BALANCE`,
 }
 export function queryKey(key: string, opts: Record<string, any>) {
-  const uriQuery = new URLSearchParams(opts)
-  return key + '?' + uriQuery.toString()
+  const uriQuery = new URLSearchParams(opts);
+  return key + "?" + uriQuery.toString();
 }
