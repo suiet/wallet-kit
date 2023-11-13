2492db889360b052fab3f91b6591e8c116489d65
brrruski <sizhe4real@gmail.com>
2023-11-12 23:31:37 -0500

    feat(kit): update useWalletDetection

--- packages/kit/src/hooks/useWalletDetection.ts
+++ packages/kit/src/hooks/useWalletDetection.ts
@@ -7,12 +7,11 @@ import { IWalletAdapter, IWalletRadar, WalletRadar } from "@suiet/wallet-sdk";
  * Notice: call once only in provider, cuz there is event registration
  */
 export function useWalletAdapterDetection() {
-  const walletRadar = useRef<IWalletRadar>();
-  // normalized adapters
+  const walletRadar = useRef<IWalletRadar | null>(null);
   const [availableWalletAdapters, setAvailableWalletAdapters] = useState<
     IWalletAdapter[]
   >([]);
-  console.log("--availableWalletAdapters", availableWalletAdapters);
+  // console.log("--availableWalletAdapters", availableWalletAdapters);
 
   useEffect(() => {
     if (!walletRadar.current) {
@@ -24,9 +23,14 @@ export function useWalletAdapterDetection() {
       walletRadar.current.getDetectedWalletAdapters();
     setAvailableWalletAdapters(initialWalletAdapters);
 
+    walletRadar.current.subscribe((newWalletAdapters) => {
+      setAvailableWalletAdapters(newWalletAdapters);
+    });
+
     return () => {
       if (walletRadar.current) {
         walletRadar.current.deactivate();
+        walletRadar.current = null;
       }
     };
   }, []);
