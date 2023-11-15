1e30ab5b7c58e964359b15a13a101beff4d3a3c1
brrruski <sizhe4real@gmail.com>
2023-11-15 00:53:21 -0500

    chore(sdk,kit): update examples

--- examples/with-next-app-router/app/page.tsx
+++ examples/with-next-app-router/app/page.tsx
@@ -1,6 +1,6 @@
-"use client";  // In Next.js, this is required to prevent the component from being rendered on the server.
+"use client"; // In Next.js, this is required to prevent the component from being rendered on the server.
 
-import Image from 'next/image'
+import Image from "next/image";
 import {
   addressEllipsis,
   ConnectButton,
@@ -8,89 +8,102 @@ import {
   formatSUI,
   SuiChainId,
   useAccountBalance,
-  useWallet
+  useWallet,
 } from "@suiet/wallet-kit";
-import {useMemo} from "react";
-import {TransactionBlock} from "@mysten/sui.js";
+import { useMemo } from "react";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
 
 const sampleNft = new Map([
-  ['sui:devnet', '0xe146dbd6d33d7227700328a9421c58ed34546f998acdc42a1d05b4818b49faa2::nft::mint'],
-  ['sui:testnet', '0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint'],
-  ['sui:mainnet', '0x5b45da03d42b064f5e051741b6fed3b29eb817c7923b83b92f37a1d2abf4fbab::nft::mint'],
-])
-
+  [
+    "sui:devnet",
+    "0xe146dbd6d33d7227700328a9421c58ed34546f998acdc42a1d05b4818b49faa2::nft::mint",
+  ],
+  [
+    "sui:testnet",
+    "0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint",
+  ],
+  [
+    "sui:mainnet",
+    "0x5b45da03d42b064f5e051741b6fed3b29eb817c7923b83b92f37a1d2abf4fbab::nft::mint",
+  ],
+]);
 
 export default function Home() {
-  const wallet = useWallet()
-  const {balance} = useAccountBalance();
+  const wallet = useWallet();
+  const { balance } = useAccountBalance();
   const nftContractAddr = useMemo(() => {
-    if (!wallet.chain) return '';
-    return sampleNft.get(wallet.chain.id) ?? '';
+    if (!wallet.chain) return "";
+    return sampleNft.get(wallet.chain.id) ?? "";
   }, [wallet]);
 
   function uint8arrayToHex(value: Uint8Array | undefined) {
-    if (!value) return ''
+    if (!value) return "";
     // @ts-ignore
-    return value.toString('hex')
+    return value.toString("hex");
   }
 
   async function handleExecuteMoveCall(target: string | undefined) {
     if (!target) return;
 
     try {
-      const tx = new TransactionBlock()
+      const tx = new TransactionBlock();
       tx.moveCall({
         target: target as any,
         arguments: [
-          tx.pure('Suiet NFT'),
-          tx.pure('Suiet Sample NFT'),
-          tx.pure('https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4')
-        ]
-      })
+          tx.pure("Suiet NFT"),
+          tx.pure("Suiet Sample NFT"),
+          tx.pure(
+            "https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4"
+          ),
+        ],
+      });
       const resData = await wallet.signAndExecuteTransactionBlock({
         transactionBlock: tx,
       });
-      console.log('executeMoveCall success', resData);
-      alert('executeMoveCall succeeded (see response in the console)');
+      console.log("executeMoveCall success", resData);
+      alert("executeMoveCall succeeded (see response in the console)");
     } catch (e) {
-      console.error('executeMoveCall failed', e);
-      alert('executeMoveCall failed (see response in the console)');
+      console.error("executeMoveCall failed", e);
+      alert("executeMoveCall failed (see response in the console)");
     }
   }
 
   async function handleSignMsg() {
-    if (!wallet.account) return
+    if (!wallet.account) return;
     try {
-      const msg = 'Hello world!'
-      const msgBytes = new TextEncoder().encode(msg)
-      const result = await wallet.signMessage({
-        message: msgBytes
-      })
-      const verifyResult = await wallet.verifySignedMessage(result, wallet.account.publicKey)
-      console.log('verify signedMessage', verifyResult)
+      const msg = "Hello world!";
+      const msgBytes = new TextEncoder().encode(msg);
+      const result = await wallet.signPersonalMessage({
+        message: msgBytes,
+      });
+      const verifyResult = await wallet.verifySignedMessage(
+        result,
+        wallet.account.publicKey
+      );
+      console.log("verify signedMessage", verifyResult);
       if (!verifyResult) {
-        alert(`signMessage succeed, but verify signedMessage failed`)
+        alert(`signMessage succeed, but verify signedMessage failed`);
       } else {
-        alert(`signMessage succeed, and verify signedMessage succeed!`)
+        alert(`signMessage succeed, and verify signedMessage succeed!`);
       }
     } catch (e) {
-      console.error('signMessage failed', e)
-      alert('signMessage failed (see response in the console)')
+      console.error("signMessage failed", e);
+      alert("signMessage failed (see response in the console)");
     }
   }
 
   const chainName = (chainId: string | undefined) => {
     switch (chainId) {
       case SuiChainId.MAIN_NET:
-        return 'Mainnet'
+        return "Mainnet";
       case SuiChainId.TEST_NET:
-        return 'Testnet'
+        return "Testnet";
       case SuiChainId.DEV_NET:
-        return 'Devnet'
+        return "Devnet";
       default:
-        return 'Unknown'
+        return "Unknown";
     }
-  }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -106,7 +119,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -128,45 +141,57 @@ export default function Home() {
           height={37}
           priority
           onClick={() => {
-            window.open('https://github.com/suiet/wallet-kit', '_blank')
+            window.open("https://github.com/suiet/wallet-kit", "_blank");
           }}
         />
 
         <ConnectButton
           onConnectError={(error) => {
             if (error.code === ErrorCode.WALLET__CONNECT_ERROR__USER_REJECTED) {
-              console.warn('user rejected the connection to ' + error.details?.wallet)
+              console.warn(
+                "user rejected the connection to " + error.details?.wallet
+              );
             } else {
-              console.warn('unknown connect error: ', error)
+              console.warn("unknown connect error: ", error);
             }
           }}
         />
 
         {!wallet.connected ? (
-          <p className={'my-8'}>Connect DApp with Suiet wallet from now!</p>
+          <p className={"my-8"}>Connect DApp with Suiet wallet from now!</p>
         ) : (
-          <div className={'my-8'}>
+          <div className={"my-8"}>
             <div>
               <p>current wallet: {wallet.adapter?.name}</p>
               <p>
-                wallet status:{' '}
+                wallet status:{" "}
                 {wallet.connecting
-                  ? 'connecting'
+                  ? "connecting"
                   : wallet.connected
-                    ? 'connected'
-                    : 'disconnected'}
+                  ? "connected"
+                  : "disconnected"}
               </p>
-              <p>wallet address: {addressEllipsis(wallet.account?.address ?? '')}</p>
+              <p>
+                wallet address: {addressEllipsis(wallet.account?.address ?? "")}
+              </p>
               <p>current network: {wallet.chain?.name}</p>
-              <p>wallet balance: {formatSUI(balance ?? 0, {
-                withAbbr: false
-              })} SUI</p>
+              <p>
+                wallet balance:{" "}
+                {formatSUI(balance ?? 0, {
+                  withAbbr: false,
+                })}{" "}
+                SUI
+              </p>
             </div>
-            <div className={'flex flex-col my-8'}>
+            <div className={"flex flex-col my-8"}>
               {nftContractAddr && (
-                <button onClick={() => handleExecuteMoveCall(nftContractAddr)}>Mint {chainName(wallet.chain?.id)} NFT</button>
+                <button onClick={() => handleExecuteMoveCall(nftContractAddr)}>
+                  Mint {chainName(wallet.chain?.id)} NFT
+                </button>
               )}
-              <button className={'mt-4'} onClick={handleSignMsg}>signMessage</button>
+              <button className={"mt-4"} onClick={handleSignMsg}>
+                signMessage
+              </button>
             </div>
           </div>
         )}
@@ -174,5 +199,5 @@ export default function Home() {
 
       <div></div>
     </main>
-  )
+  );
 }
