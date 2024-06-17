ffccfd3137b79d0141939356e640fd64e39a4e25
brrruski <sizhe4real@gmail.com>
2024-06-17 09:29:10 -0400

    feat: support new hook useSuiClient

--- packages/kit/src/hooks/useSuiProvider.ts
+++ packages/kit/src/hooks/useSuiProvider.ts
@@ -3,6 +3,10 @@ import { useMemo } from "react";
 
 export type SuiProvider = SuiClient;
 
+/**
+ * Return a SuiClient instance.
+ * @deprecated Use `useSuiClient` instead.
+ */
 export function useSuiProvider(endpoint: string): SuiProvider {
   return useMemo<SuiClient>(() => new SuiClient({ url: endpoint }), [endpoint]);
 }
