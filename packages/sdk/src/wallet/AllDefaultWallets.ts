205a49c22223fe3920128da6ac772ad9a78467e5
brrruski <sizhe4real@gmail.com>
2025-04-26 09:04:43 -0400

    feat: maintain wallet presets

--- packages/sdk/src/wallet/AllDefaultWallets.ts
+++ packages/sdk/src/wallet/AllDefaultWallets.ts
@@ -3,19 +3,15 @@ import * as presets from "./preset-wallets";
 export const AllDefaultWallets = [
   presets.SuietWallet,
   presets.SlushWallet,
+  presets.PhantomWallet,
+  // sorted by name alphabetically
   ...[
-    presets.EthosWallet,
     presets.MartianWallet,
     presets.SurfWallet,
-    presets.MorphisWallet,
     presets.GlassWallet,
     presets.OneKeyWallet,
-    presets.SpacecyWallet,
-    presets.SensuiWallet,
     presets.NightlyWallet,
-    presets.ElliWallet,
     presets.TokenPocketWallet,
-    presets.FrontierWallet,
-    presets.PhantomWallet,
+    presets.OkxWallet,
   ].sort((a, b) => (a.name < b.name ? -1 : 1)),
 ];
